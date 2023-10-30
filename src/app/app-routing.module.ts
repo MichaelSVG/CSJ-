20ade9fcdc3539e645bb0fe1927a3fcb5a909b06
MichaelSVG <128263329+MichaelSVG@users.noreply.github.com>
2023-10-30 15:37:59 -0500

    Proyecto CSJ
    Angular

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+
+const Routes: Routes =[
+  {path:'',redirectTo:'/login',pathMatch:'full'},
+  {path:'login', component: AuthComponent },
+  {path:'dashboard',
+  children:[
+      {path:'Usuario',
+        loadChildren: () => import('./Usuario/usuario.module').then(m => m.UsuarioModule)
+      }
+    ]
+  }
+]
+
+@NgModule({
+  imports: [RouterModule.forRoot(Routes)],
+  exports:[RouterModule]
+
+})
+export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component ,ViewChild } from '@angular/core';
+import { MatDrawer } from '@angular/material/sidenav';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CSJ';
+
+  @ViewChild(MatDrawer)
+  sidenav!: MatDrawer;
+
+  constructor(private observer: BreakpointObserver, private cd: ChangeDetectorRef){
+
+  }
+  
+    ngAfterViewInit(){
+
+      this.observer.observe(['(max-width: 800px)']).subscribe((resp: any) => {
+        if(resp.matches){
+          this.sidenav.mode = 'over';
+          this.sidenav.close();
+        }else{
+          this.sidenav.mode = 'side';
+          this.sidenav.open();
+        }
+      })
+      this.cd.detectChanges;
+    }
+ 
 }
